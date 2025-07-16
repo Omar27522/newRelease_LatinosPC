9558babd468b2fa1f5b229e9719f44648fb3b0a1
Omar27522 <72665821+Omar27522@users.noreply.github.com>
2025-07-15 18:03:17 -0700

    update

--- LAtPC/index.php
+++ LAtPC/index.php
@@ -46,177 +46,175 @@ Built with user guidance and clarity in mind — because your time and tech matt
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>LatinosPC - Tech Support & Computer Services in Pomona, CA</title>
-  <link rel="stylesheet" href="mainStyle.css">
-  <link rel="stylesheet" href="homepage.css">
-  <link rel="stylesheet" href="terwanpop.css">
-  <link rel="stylesheet" href="logo.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LatinosPC - Tech Support & Computer Services in Pomona, CA</title>
+    <link rel="stylesheet" href="mainStyle.css">
+    <link rel="stylesheet" href="homepage.css">
+    <link rel="stylesheet" href="terwanpop.css">
+    <link rel="stylesheet" href="logo.css">
 </head>
+
 <body>
-<header>
-            <!-- <span class="crumbs">
+    <header>
+        <!-- <span class="crumbs">
                 <?php #$page->getCrumbs(); ?>
                 <hr>
             </span> -->
-            <a href="#" class="phone">
-                909-276-7214
-            </a>
-            <div class="logo">
+        <a href="#" class="phone">
+            909-276-7214
+        </a>
+        <div class="logo">
             <div class="logos"><span>LA</span>tinos<span>PC</span>.com
                 <small class="rtl">
-                  PC is for Personal Computer
+                    PC is for Personal Computer
                 </small>
             </div>
-            </div>
-            <nav class="sticky">
-                <!--    TerwanPOP    -->
-                <div role="navigation" class="burg">
-                    <div id="menuToggle"><input type="checkbox" />
-                        <span></span><span></span><span></span>
-                        
-<ul id="menu">
-    <li class="titleli">Our Lord and Savior Jesus Christ</li>
-    <a href="https://www.vatican.va/roman_curia/pontifical_councils/interelg/documents/rc_pc_interelg_doc_20030203_new-age_en.html"
-        target="_blank" alt="Jesus">
-        <img src="https://latinospc.com/images/artificialintelligence/our_lord_and_savior_jesus_christ/lamb9.webp"
-            width="100%" height="auto">
-    </a>
-    <a href="#">
-        <li>Heavenly Father Tell me about the Web</li>
-    </a><a href="#">
-        <!-- https://bibilium.com/38-parables-of-Jesus-parables-of-Jesus/ -->
-        <li>Parables</li>
-    </a><a href="#">
-        <li>Sermons/Discourses</li>
-    </a><a href="#">
-        <li>The Apostles</li>
-    </a><a href="#">
-        <li>His Teachings</li>
-    </a><a href="#">
-        <li>Computer Backup</li>
-    </a><a href="#">
-        <li>Hardware/Parts</li>
-    </a><a href="#">
-        <li>Software/Programs</li>
-    </a><a href="#">
-        <li>Websites</li>
-    </a><a href="#">
-        <li>Security</li>
-    </a><a href="#">
-        <li>Best Practices</li>
-    </a><a href="#">
-        <li>Passwords</li>
-    </a><a href="#">
-        <li>Gallery</li>
-    </a>
-</ul>
-                    </div>
+        </div>
+        <nav class="sticky">
+            <!--    TerwanPOP    -->
+            <div role="navigation" class="burg">
+                <div id="menuToggle"><input type="checkbox" />
+                    <span></span><span></span><span></span>
+
+                    <ul id="menu">
+                        <li class="titleli">Our Lord and Savior Jesus Christ</li>
+                        <a href="https://www.vatican.va/roman_curia/pontifical_councils/interelg/documents/rc_pc_interelg_doc_20030203_new-age_en.html"
+                            target="_blank" alt="Jesus">
+                            <img src="https://latinospc.com/images/artificialintelligence/our_lord_and_savior_jesus_christ/lamb9.webp"
+                                width="100%" height="auto">
+                        </a>
+                        <a href="#">
+                            <li>Heavenly Father Tell me about the Web</li>
+                        </a><a href="#">
+                            <!-- https://bibilium.com/38-parables-of-Jesus-parables-of-Jesus/ -->
+                            <li>Parables</li>
+                        </a><a href="#">
+                            <li>Sermons/Discourses</li>
+                        </a><a href="#">
+                            <li>The Apostles</li>
+                        </a><a href="#">
+                            <li>His Teachings</li>
+                        </a><a href="#">
+                            <li>Computer Backup</li>
+                        </a><a href="#">
+                            <li>Hardware/Parts</li>
+                        </a><a href="#">
+                            <li>Software/Programs</li>
+                        </a><a href="#">
+                            <li>Websites</li>
+                        </a><a href="#">
+                            <li>Security</li>
+                        </a><a href="#">
+                            <li>Best Practices</li>
+                        </a><a href="#">
+                            <li>Passwords</li>
+                        </a><a href="#">
+                            <li>Gallery</li>
+                        </a>
+                    </ul>
                 </div>
-                <div class="navButtons">
+            </div>
+            <div class="navButtons">
                 <!--    TerwanPOP Made by Erik Terwan    -->
                 <button><a href="#">Home</a></button>
                 <button><a href="#">Español</a></button>
                 <button><a href="#">Services</a></button>
                 <button><a href="#">Contact Us</a></button>
                 <button><a href="#">Reviews</a></button>
-                </div>
-            </nav>
-            </header>
+            </div>
+        </nav>
+    </header>
 
-<main>
-  
+    <main>
 
+        <section class="homepage-grid">
+            <div class="grid-item">
+                <h3>Jesus Christ</h3>
+                <a href="#" class="cta-button">Tell me about the web</a>
+                <a href="#" class="cta-button">Apostles</a>
+                <a href="#" class="cta-button">Parables</a>
+                <a href="#" class="cta-button">Sermons</a>
+                <a href="#" class="cta-button">Teachings</a>
+            </div>
 
-<section class="homepage-grid">
-    <div class="grid-item">
-      <h3>Jesus Christ</h3>
-      <a href="#" class="cta-button">Tell me about the web</a>
-      <a href="#" class="cta-button">Apostles</a>
-      <a href="#" class="cta-button">Parables</a>
-      <a href="#" class="cta-button">Sermons</a>
-      <a href="#" class="cta-button">Teachings</a>
-    </div>
+            <div class="grid-item">
+                <h3>About Us</h3>
+                <p>we want to bring families and individuals, the experience of a fully working computer system; laptops
+                    and desktops.</p>
+                <p>100% Satisfaction Guarantee | 90 Days Service Package -included</p>
+                <p>We are located in the city of Pomona, and will travel to LA county, SB county, RV county and OC.</p>
+                <a href="#" class="cta-button">Visit Page</a>
+            </div>
 
-    <div class="grid-item">
-      <h3>About Us</h3>
-      <p>we want to bring families and individuals, the experience of a fully working computer system; laptops and desktops.</p>
-      <p>100% Satisfaction Guarantee | 90 Days Service Package -included</p>
-      <p>We are located in the city of Pomona, and will travel to LA county, SB county, RV county and OC.</p>
-      <a href="#" class="cta-button">Visit Page</a>
-    </div>
+            <div class="grid-item">
+                <h3>Services</h3>
+                <a href="#" class="cta-button">Cleanups</a>
+                <a href="#" class="cta-button">Computer protection</a>
+                <a href="#" class="cta-button">Consultations</a>
+                <a href="#" class="cta-button">Managed Services</a>
+                <a href="#" class="cta-button">Remote Support</a>
+                <a href="#" class="cta-button">Websites</a>
+            </div>
 
-    <div class="grid-item">
-      <h3>Services</h3>
-      <a href="#" class="cta-button">Cleanups</a>
-      <a href="#" class="cta-button">Computer protection</a>
-      <a href="#" class="cta-button">Consultations</a>
-      <a href="#" class="cta-button">Managed Services</a>
-      <a href="#" class="cta-button">Remote Support</a>
-      <a href="#" class="cta-button">Websites</a>
-    </div>
+            <div class="grid-item">
+                <h3>Security</h3>
+                <a href="#" class="cta-button">Backups</a>
+                <a href="#" class="cta-button">Passwords</a>
+                <a href="#" class="cta-button">Best Practices</a>
+                <aside>
+                    <hr>
+                    <h3>Tutorials / Blog</h3>
+                    <p>Visit our tutorials and blog page for more information</p>
+                    <a href="#" class="cta-button">Visit Page</a>
+                </aside>
+            </div>
 
-    <div class="grid-item">
-      <h3>Security</h3>
-      <a href="#" class="cta-button">Backups</a>
-      <a href="#" class="cta-button">Passwords</a>
-      <a href="#" class="cta-button">Best Practices</a>
-      <aside>
-        <hr>
-      <h3>Tutorials / Blog</h3>
-        <p>Visit our tutorials and blog page for more information</p>
-        <a href="#" class="cta-button">Visit Page</a>
-      </aside>
-    </div>
+            <div class="grid-item">
+                <h3>Hardware</h3>
+                <a href="#" class="cta-button">CPU</a>
+                <a href="#" class="cta-button">Storage</a>
+                <a href="#" class="cta-button">Power Supply Unit</a>
+                <a href="#" class="cta-button">Input</a>
+                <a href="#" class="cta-button">Output</a>
+                <a href="#" class="cta-button">Parts</a>
 
-    <div class="grid-item">
-      <h3>Hardware</h3>
-      <a href="#" class="cta-button">CPU</a>
-      <a href="#" class="cta-button">Storage</a>
-      <a href="#" class="cta-button">Power Supply Unit</a>
-      <a href="#" class="cta-button">Input</a>
-      <a href="#" class="cta-button">Output</a>
-      <a href="#" class="cta-button">Parts</a>
+                <aside>
+                    <hr>
+                    <div>
+                        <h3>Peripherals and Gear</h3>
+                        <p>Visit our computer peripherals and gear page for more information</p>
+                        <a href="#" class="cta-button">Visit Page</a>
+                    </div>
+                </aside>
+            </div>
 
-      <aside>
-        <hr>
-        <div>
-          <h3>Peripherals and Gear</h3>
-          <p>Visit our computer peripherals and gear page for more information</p>
-          <a href="#" class="cta-button">Visit Page</a>
-        </div>
-      </aside>
-    </div>
+            <div class="grid-item">
+                <h3>Software</h3>
+                <a href="#" class="cta-button">Anti-Virus</a>
+                <a href="#" class="cta-button">Malware Cleaner</a>
+                <a href="#" class="cta-button">Web Browsers</a>
+                <a href="#" class="cta-button">Messaging</a>
+                <a href="#" class="cta-button">Media</a>
+                <a href="#" class="cta-button">Runtimes</a>
+                <a href="#" class="cta-button">Imaging</a>
+                <a href="#" class="cta-button">Documents</a>
+                <a href="#" class="cta-button">File Sharing</a>
+                <a href="#" class="cta-button">Online Storage</a>
+                <a href="#" class="cta-button">Other</a>
+                <a href="#" class="cta-button">Utilities</a>
+                <a href="#" class="cta-button">Compression</a>
+                <a href="#" class="cta-button">Developer Tools</a>
+            </div>
+        </section>
+    </main>
 
-    <div class="grid-item">
-      <h3>Software</h3>
-      <a href="#" class="cta-button">Anti-Virus</a>
-      <a href="#" class="cta-button">Malware Cleaner</a>
-      <a href="#" class="cta-button">Web Browsers</a>
-      <a href="#" class="cta-button">Messaging</a>
-      <a href="#" class="cta-button">Media</a>
-      <a href="#" class="cta-button">Runtimes</a>
-      <a href="#" class="cta-button">Imaging</a>
-      <a href="#" class="cta-button">Documents</a>
-      <a href="#" class="cta-button">File Sharing</a>
-      <a href="#" class="cta-button">Online Storage</a>
-      <a href="#" class="cta-button">Other</a>
-      <a href="#" class="cta-button">Utilities</a>
-      <a href="#" class="cta-button">Compression</a>
-      <a href="#" class="cta-button">Developer Tools</a>
-    </div>
-
-
-
-
-  </section>
-</main>
-
-<footer>
-  <p>&copy; 2025 LatinosPC - Your Trusted Tech Partner in Pomona, CA</p>
-</footer>
+    <footer>
+        <p>&copy; 2025 LatinosPC - Your Trusted Tech Partner in Pomona, CA</p>
+    </footer>
 
 </body>
+
 </html>
