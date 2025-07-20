6642a7a86f7ae71995b3e1b6fd4b7e5d6ccb0444
Omar27522 <72665821+Omar27522@users.noreply.github.com>
2025-07-19 21:41:49 -0700

    homepage: create initial website layout with responsive design and dialog system

--- LAtPC/index.php
+++ LAtPC/index.php
@@ -92,8 +92,8 @@
 
 
 
-  
-<!--
+
+        <!--
         <div class="multi-link-container">
             <span class="linked-text">Click me for more information</span>
             <div class="link-dialog">
@@ -122,7 +122,14 @@
 
         <section class="homepage-grid">
             <div class="grid-item">
-                <h3>Jesus Christ</h3>
+                <div class="multi-link-container">
+                    <h3 class="linked-text">Jesus Christ</h3>
+                    <div class="link-dialog">
+                        <p tabindex="0">Most accept that Jesus was truly a man who lived in Israel 2,000 years ago.</p>
+                        <p tabindex="0">What do we really know about Jesus? Explore his life and teachings and form your
+                            own valid experience.</p>
+                    </div>
+                </div>
                 <a href="#content" class="cta-button">Tell me about the web</a>
                 <a href="#" class="cta-button">Apostles</a>
                 <a href="#" class="cta-button">Parables</a>
@@ -132,10 +139,16 @@
 
             <div class="grid-item">
                 <div class="multi-link-container">
-                <h3 class="linked-text">About Us</h3>
+                    <h3 class="linked-text">About Us</h3>
                     <div class="link-dialog">
-                        <p tabindex="0">We
-                        want to bring families and individuals, the experience of a fully working computer system.</p>
+                        <p tabindex="0">
+                            We started back in 2009 with one technician; Omar, we have one goal.</p>
+                        <p tabindex="0">
+                            Our goal is to provide you and your company with outstanding service.</p>
+                        <p tabindex="0">We're dedicated to enhancing your experience through effective marketing,
+                            customer-friendly retention
+                            strategies, and maintaining top-notch quality control.
+                        </p>
                     </div>
                 </div>
 
@@ -147,7 +160,18 @@
             </div>
 
             <div class="grid-item">
-                <h3>Services</h3>
+                <div class="multi-link-container">
+                    <h3 class="linked-text">Services</h3>
+                    <div class="link-dialog">
+                        <p tabindex="0">We mostly work with individuals, Students, Small-Medium Businesses, #Gamers!,
+                            and Professionals.</p>
+                        <p tabindex="0">Repairs and Service for Home and Office; any type-spec.</p>
+                        <p tabindex="0">90 day service guarantee. Computers run smoother, faster and perform better.</p>
+                        <img src="https://latinospc.com/images/number.jpg" width="auto" height="auto" alt="number"
+                            style="display: block; margin: 0 auto;">
+                    </div>
+                </div>
+
                 <a href="#" class="cta-button">Cleanups</a>
                 <a href="#" class="cta-button">Computer protection</a>
                 <a href="#" class="cta-button">Consultations</a>
@@ -157,20 +181,45 @@
             </div>
 
             <div class="grid-item">
-                <h3>Security</h3>
+                <div class="multi-link-container">
+                    <h3 class="linked-text">Security</h3>
+                    <div class="link-dialog">
+                        <p tabindex="0">Think of your computer as a virtual treasure chest filled with your important
+                            stuff – photos, documents, and more. Computer protection is like a shield against
+                            troublemakers like viruses and hackers.</p>
+                    </div>
+                </div>
+
+                <a href="#" class="cta-button">Best Practices</a>
                 <a href="#" class="cta-button">Backups</a>
                 <a href="#" class="cta-button">Passwords</a>
-                <a href="#" class="cta-button">Best Practices</a>
                 <aside>
                     <hr>
-                    <h3>Tutorials / Blog</h3>
+                    <div class="multi-link-container">
+                        <h3 class="linked-text">Tutorials / Blog</h3>
+                        <div class="link-dialog">
+                            <p tabindex="0">This section is all about the tutorials and blog posts that we have on our
+                                website. We have a wide range of topics that we cover, from computer hardware and
+                                software to internet security and more.</p>
+                        </div>
+                    </div>
                     <p>Visit our tutorials and blog page for more information</p>
                     <a href="#" class="cta-button">Visit Page</a>
                 </aside>
             </div>
 
             <div class="grid-item">
-                <h3>Hardware</h3>
+                <div class="multi-link-container">
+                    <h3 class="linked-text">Hardware</h3>
+                    <div class="link-dialog">
+                        <p tabindex="0">A computer is an electronic device that manipulates data or information. It has
+                            the ability to store,retrieve, and process data.</p>
+                        <p tabindex="0">The Hardware is the computer — it's physical components.</p>
+                        <p>These components may include the computer's functionality, design, materials, innovations,
+                            thermals, etc.</p>
+                    </div>
+                </div>
+
                 <a href="#" class="cta-button">CPU</a>
                 <a href="#" class="cta-button">Storage</a>
                 <a href="#" class="cta-button">Power Supply Unit</a>
@@ -180,16 +229,29 @@
 
                 <aside>
                     <hr>
-                    <div>
-                        <h3>Peripherals and Gear</h3>
-                        <p>Visit our computer peripherals and gear page for more information</p>
-                        <a href="#" class="cta-button">Visit Page</a>
+                    <div class="multi-link-container">
+                        <h3 class="linked-text">Peripherals and Gear</h3>
+                        <div class="link-dialog">
+                            <p tabindex="0">Peripherals and gear are the additional components that help you use your
+                                computer.</p>
+                            <p tabindex="0">These include desks, chairs, computer mats, lights, clothing, and other
+                                stuff that are unrelated to the computer itself, but are very useful to have.</p>
+                        </div>
                     </div>
-                </aside>
+                    <p>Visit our computer peripherals and gear page for more information</p>
+                    <a href="#" class="cta-button">Visit Page</a>
+            </div>
+            </aside>
             </div>
 
             <div class="grid-item">
-                <h3>Software</h3>
+                <div class="multi-link-container">
+                    <h3 class="linked-text">Software</h3>
+                    <div class="link-dialog">
+                        <p tabindex="0">Commonly known as computer programs or apps, these are the instructions for the
+                            computer’s tasks</p>
+                    </div>
+                </div>
                 <a href="#" class="cta-button">Anti-Virus</a>
                 <a href="#" class="cta-button">Malware Cleaner</a>
                 <a href="#" class="cta-button">Web Browsers</a>
